933bdac9f3f87553a07a5c45e3af45cc08c190bf
myildirim48 <muhammedyildirim48@gmail.com>
2022-09-05 20:36:05 +0300

    Add Firebase Login,Register,Log Out - Use Constants File

--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -7,23 +7,27 @@
 //
 
 import UIKit
+import CLTypingLabel
 
 class WelcomeViewController: UIViewController {
 
-    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var titleLabel: CLTypingLabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        titleLabel.text = ""
-        let titleText = "⚡️FlashChat"
-        var charIndex = 0.0
-        for letter in titleText {
-            Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex, repeats: false) { timer in
-                self.titleLabel.text?.append(letter)
-            }
-            charIndex += 1
-        }
+        titleLabel.text = K.appName //Animation with cocoapods CLTypingLabel
+        
+        //Manuel Animation
+//        titleLabel.text = ""
+//        let titleText = "⚡️FlashChat"
+//        var charIndex = 0.0
+//        for letter in titleText {
+//            Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex, repeats: false) { timer in
+//                self.titleLabel.text?.append(letter)
+//            }
+//            charIndex += 1
+//        }
     }
     
 
