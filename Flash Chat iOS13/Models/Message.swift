aed24e4b1081a9a31b6af3863461a798c2be2070
myildirim48 <muhammedyildirim48@gmail.com>
2022-09-05 22:22:45 +0300

    Add UITable and Create Message Model, Customize Cells using a .xib File, Add Firestore

---
+++ Flash Chat iOS13/Models/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by YILDIRIM on 5.09.2022.
+//  Copyright © 2022 Angela Yu. All rights reserved.
+//
+
+import Foundation
+struct  Message {
+    let sender: String //Email address
+    let body: String //Message text
+    
+}
